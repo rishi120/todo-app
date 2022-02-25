2746072f215ba225dee0e172728af6ea7c6404ab
chandan <chandan.b@pacewisdom.com>
2022-02-24 22:29:53 -0800

    added react usecontext functionalities.

--- src/components/child-components/task.jsx
+++ src/components/child-components/task.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { data } from "../master";
 
 export default function Addtask() {
+    const { inputFocus, handleInput } = useContext(data);
     return (
         <div className='lg:w-[100%] lg:flex lg:justify-center'>
             <div className='lg:w-[500px] border-2 border-white rounded-md mt-5 p-6 shadow z-10'>
-                <input type="text" placeholder='Add Task' className='lg:w-[100%] block p-4 border-2 border-blue-500 rounded-md mb-5 shadow-md placeholder:text-sm placeholder:font-mono placeholder:text-gray-400' />
+                <input type="text" placeholder='Add Task' className='lg:w-[100%] block p-4 border-2 border-blue-500 rounded-md mb-5 shadow-md placeholder:text-sm placeholder:font-mono placeholder:text-gray-400' ref={inputFocus} onChange={(e) => handleInput(e)} />
                 <button type='button' className='lg:w-40 bg-blue-600 shadow-lg p-2 rounded-md text-white font-mono'>Add</button>
             </div>
         </div>
--- src/components/master.jsx
+++ src/components/master.jsx
@@ -1,14 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect, createContext, useRef } from 'react'
 import Todo from "./child-components/todo";
 
+const data = createContext();
+
 function Mastercomponent() {
+    const inputFocus = useRef();
+
+    const handleInput = (event) => {
+        console.log(event.target.value);
+    }
+
+    useEffect(() => {
+        setTimeout(() => {
+            inputFocus.current.focus();
+        }, 300);
+
+    }, [])
+
+    const inputComponentValues = {
+        inputFocus,
+        handleInput
+    }
+
     return (
         <section className='p-4'>
             <h1 className=' pb-3 font-sans text-2xl font-semibold'>A simple Todo App</h1>
             <hr />
-            <Todo />
+            <data.Provider value={inputComponentValues}>
+                <Todo />
+            </data.Provider>
         </section>
     )
 }
 
 export default Mastercomponent
+export { data };
